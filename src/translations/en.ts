ee8d07833a086c769212bff0f320d2bf63f2687c
Kasper Schmidt Johansen <60236163+kschmidtjohansen@users.noreply.github.com>
2025-05-07 18:47:10 +0200

    Rettelse i Translation

--- src/translations/en.ts
+++ src/translations/en.ts
@@ -30,8 +30,8 @@ const translations = {
     buttonLoading: 'Logging in...',
     failed: 'Invalid email or password. Please try again.',
     success: 'Login successful',
-    welcomeMessage: 'Welcome to Polygon Weekly Compass',
-    internalSystem: 'Internal Planning System',
+    welcomeMessage: 'Welcome to Polygon Weekly Planner',
+    internalSystem: 'Afd. 12',
     testCredentials: 'For testing, use the following credentials:',
     passwordReset: {
       forgotPassword: 'Forgot password?',
